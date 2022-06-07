07c86730c5897a844e8313b3791393e11390cfbc
Nicholas Merrell <105929264+merrelly@users.noreply.github.com>
2022-06-07 13:20:43 +0100

    Create enumerable-magic-#25.js

---
+++ 8-kyu/enumerable-magic-#25.js
@@ -0,0 +1,9 @@
+// ENUMERABLE MAGIC #25 - TAKE THE FIRST N ELEMENTS
+// https://www.codewars.com/kata/545afd0761aa4c3055001386
+
+
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+
+// MY SOLUTION
+let take = (arr, n) => arr.slice(0, n);
