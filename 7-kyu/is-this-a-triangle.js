34cc952446c71e3d23a3ad9d7de135c738963580
Nicholas Merrell <105929264+merrelly@users.noreply.github.com>
2022-06-14 11:28:54 +0100

    Create is-this-a-triangle.js

---
+++ 7-kyu/is-this-a-triangle.js
@@ -0,0 +1,11 @@
+// IS THIS A TRIANGLE?
+// https://www.codewars.com/kata/56606694ec01347ce800001b
+
+
+/* Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted). */
+
+
+// MY SOLUTION
+const isTriangle = (a,b,c) => a + b > c && a + c > b && b + c > a ? true : false;
