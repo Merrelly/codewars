ee89066b14c67c9c3a1ff8f49cf1a5465edf05fc
Nicholas Merrell <105929264+merrelly@users.noreply.github.com>
2022-08-05 16:48:53 +0100

    Create convert-boolean-values.js

---
+++ 8-kyu/convert-boolean-values.js
@@ -0,0 +1,13 @@
+// CONVERT BOOLEAN VALUES TO STRINGS 'YES' OR 'NO'
+// https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+// MY SOLUTION
+function boolToWord( bool ){
+  if(bool === true){
+    return "Yes"
+  }else return "No"
+}
