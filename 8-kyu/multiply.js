ab396568e3a3d757282f34a27fdd148c7aa01dcc
Nicholas Merrell <105929264+merrelly@users.noreply.github.com>
2022-08-05 16:42:09 +0100

    Create multiply.js

---
+++ 8-kyu/multiply.js
@@ -0,0 +1,11 @@
+// MULTIPLY
+// https://www.codewars.com/kata/50654ddff44f800200000004
+
+
+// This code does not execute properly. Try to figure out why.
+
+
+// MY SOLUTION
+function multiply(a, b){
+  return a * b
+}
