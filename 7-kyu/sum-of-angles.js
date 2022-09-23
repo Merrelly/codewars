c77ffa4a31ae181892fc287e0158719441cbb56a
Nicholas Merrell <105929264+merrelly@users.noreply.github.com>
2022-09-23 23:36:33 +0100

    Create sum-of-angles.js

---
+++ 7-kyu/sum-of-angles.js
@@ -0,0 +1,11 @@
+// SUM OF ANGLES
+// https://www.codewars.com/kata/5a03b3f6a1c9040084001765
+
+
+// Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+
+// MY SOLUTION
+function angle(n) {
+  return (n - 2) * 180;
+}
