22751a1489aca766eec254de662de3a6f4618d2c
Nicholas Merrell <105929264+merrelly@users.noreply.github.com>
2022-10-01 16:10:57 +0100

    Create love-vs-friendship.js

---
+++ 7-kyu/love-vs-friendship.js
@@ -0,0 +1,53 @@
+// LOVE VS FRIENDSHIP
+// https://www.codewars.com/kata/59706036f6e5d1e22d000016
+
+
+/* If　a = 1, b = 2, c = 3 ... z = 26
+
+Then l + o + v + e = 54
+
+and f + r + i + e + n + d + s + h + i + p = 108
+
+So friendship is twice stronger than love :-)
+
+The input will always be in lowercase and never be empty. */
+
+
+// MY SOLUTION
+function wordsToMarks(string){
+  const alphabetValues = {
+    'a' : 1,
+    'b' : 2,
+    'c' : 3,
+    'd' : 4,
+    'e' : 5,
+    'f' : 6,
+    'g' : 7,
+    'h' : 8,
+    'i' : 9,
+    'j' : 10,
+    'k' : 11,
+    'l' : 12,
+    'm' : 13,
+    'n' : 14,
+    'o' : 15,
+    'p' : 16,
+    'q' : 17,
+    'r' : 18,
+    's' : 19,
+    't' : 20,
+    'u' : 21,
+    'v' : 22,
+    'w' : 23,
+    'x' : 24,
+    'y' : 25,
+    'z' : 26,
+  }
+  let sum = 0;
+  string.split('').forEach(e => {
+    if(e in alphabetValues){
+      sum += alphabetValues[e];
+    }
+  })
+  return sum;
+}
