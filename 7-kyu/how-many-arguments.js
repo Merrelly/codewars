27e543c682062551f29577a26b3f1bfca5f9da50
Nicholas Merrell <105929264+merrelly@users.noreply.github.com>
2022-11-27 22:49:17 +0000

    Create how-many-arguments.js

---
+++ 7-kyu/how-many-arguments.js
@@ -0,0 +1,12 @@
+// HOW MANY ARGUMENTS
+// https://www.codewars.com/kata/5c44b0b200ce187106452139
+
+
+// args_count(1, 2, 3) -> 3
+// args_count(1, 2, 3, 10) -> 4
+
+
+// MY SOLUTION
+function args_count(){
+  return arguments.length;
+}
